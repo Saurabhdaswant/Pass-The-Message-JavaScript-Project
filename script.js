67f580c6bd6b2c3a60d21d74fa53a4d642049047
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-14 16:57:49 +0530

    comments

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+"use strict";
+
 const input = document.getElementById("input");
-let message = document.querySelector(".message");
+const message = document.querySelector(".message");
 const btn = document.querySelector(".btn");
 
+//function for displaying the input on DOM
 const displayMessage = function () {
   let messageInput = document.querySelector("input").value;
   message.textContent = messageInput;
 };
 
+//function displayMessage will run after clicking on button
 document.querySelector(".btn").addEventListener("click", displayMessage);
 
+//and after pressing ENTER it will run function displayMessage
 document.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     displayMessage();
 
+    //input will be cleared after displaying the input
     document.querySelector(".input").value = "";
   }
 });
